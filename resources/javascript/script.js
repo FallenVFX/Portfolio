d70d5ba434f2c415bb7b67db1fcb7a8a1164279a
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2021-08-11 00:36:33 -0700

    Added projects to projects page and made adjustments to many parts of the website

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,16 +1,19 @@
 let menuArrow = document.getElementById("menuArrow");
+let nav = document.getElementById("nav");
 let span = [document.getElementById("home"), document.getElementById("projects"), document.getElementById("contact2")];
 
 function showMenu() {
     for (let i = 0; i < span.length; i++) {
-        if (span[i].style.display === "block") {
+        if (span[i].style.display === "inline-block") {
             span[i].style.display = "none";
             menuArrow.style.transform = "rotate(0deg)";
-            menuArrow.style.margin = "15px 50px 15px 10px";
+            menuArrow.style.margin = "10px 45px 10px 5px";
+            nav.style.width = "75px";
         } else {
-            span[i].style.display = "block";
+            span[i].style.display = "inline-block";
             menuArrow.style.transform = "rotate(180deg)";
-            menuArrow.style.margin = "15px 10px 15px 98px";
+            menuArrow.style.margin = "10px 5px 10px 73px";
+            nav.style.width = "110px";
         }
     }
 }
