fbb87bddf2d0a13809e2845be7dedfce3d8bea62
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2021-10-18 22:22:37 -0700

    First Testing

---
+++ resources/javascript/carousel.js
@@ -0,0 +1,18 @@
+const carousel = document.getElementById('carousel');
+const images = carousel.querySelectorAll('img');
+
+let i = 0;
+
+setInterval(() => {
+	if (i === 0) {
+		images[i].style.display = 'block';
+	} else if (i === images.length) {
+		images[i - 1].style.display = 'none';
+		images[0].style.display = 'block';
+		i = 0;
+	} else {
+		images[i - 1].style.display = 'none';
+		images[i].style.display = 'block';
+	}
+	i++;
+}, 6000);
--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,21 +1,17 @@
-let menuArrow = document.getElementById("menuArrow");
-let nav = document.getElementById("nav");
-let span = [document.getElementById("home"), document.getElementById("projects"), document.getElementById("contact2")];
+const apiKey = 'cUtdvQPAvijPZuYKdT2kGWdp91V7DdP9';
+const url = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}
+&size=400,300&center=Edmonds,WA&zoom=7&
+shape=border:ff0000|fill:ff0000|47.850,-122.400|47.850,-122.250|47.700,-122.325|47.850,-122.400`;
+const img = document.querySelector('.maps');
 
-function showMenu() {
-    for (let i = 0; i < span.length; i++) {
-        if (span[i].style.display === "inline-block") {
-            span[i].style.display = "none";
-            menuArrow.style.transform = "rotate(0deg)";
-            menuArrow.style.margin = "10px 45px 10px 5px";
-            nav.style.width = "75px";
-        } else {
-            span[i].style.display = "inline-block";
-            menuArrow.style.transform = "rotate(180deg)";
-            menuArrow.style.margin = "10px 5px 10px 73px";
-            nav.style.width = "110px";
-        }
-    }
-}
-
-menuArrow.onclick = showMenu;
+fetch(url)
+	.then(function (response) {
+		if (!response.ok) {
+			console.log('error');
+		}
+		return response.blob();
+	})
+	.then(function (data) {
+		let objectURL = URL.createObjectURL(data);
+		img.src = objectURL;
+	});
