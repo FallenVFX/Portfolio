51ca34d404bd8b9470b01c171aed5fbc5c08e7c2
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-10-13 15:50:42 -0700

    updated API call to return proper marker on a more zoomed in map

--- resources/javascript/mapQuest.js
+++ resources/javascript/mapQuest.js
@@ -1,17 +1,16 @@
+// Don't steal my map quest key please ;D
 const apiKey = 'cUtdvQPAvijPZuYKdT2kGWdp91V7DdP9';
-const url = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}
-&size=400,300&center=Edmonds,WA&zoom=7&
-shape=border:ff0000|fill:ff0000|47.850,-122.400|47.850,-122.250|47.700,-122.325|47.850,-122.400`;
-const img = document.querySelector('.maps');
+const url = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}&locations=Edmonds,WA&size=400,300&defaultMarker=marker-sm&zoom=8`;
+const img = document.getElementById('map');
 
 fetch(url)
-	.then(function (response) {
-		if (!response.ok) {
-			console.log('error');
-		}
-		return response.blob();
-	})
-	.then(function (data) {
-		let objectURL = URL.createObjectURL(data);
-		img.src = objectURL;
-	});
+  .then(function (response) {
+    if (!response.ok) {
+      console.log('error');
+    }
+    return response.blob();
+  })
+  .then(function (data) {
+    let objectURL = URL.createObjectURL(data);
+    img.src = objectURL;
+  });
