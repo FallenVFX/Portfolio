98baee79cb1831ae0a8e78b73f2b3cbb3f324707
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2023-06-12 19:22:54 -0700

    resdesign ready for stage 1

--- resources/javascript/mapQuest.js
+++ resources/javascript/mapQuest.js
@@ -1,6 +1,6 @@
 // Don't steal my map quest key please ;D
 const apiKey = 'cUtdvQPAvijPZuYKdT2kGWdp91V7DdP9';
-const url = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}&locations=Edmonds,WA&size=400,300&defaultMarker=marker-sm&zoom=8`;
+const url = `https://www.mapquestapi.com/staticmap/v5/map?key=${apiKey}&locations=Everett,WA&size=400,300&defaultMarker=marker-sm&zoom=8`;
 const img = document.getElementById('map');
 
 fetch(url)
